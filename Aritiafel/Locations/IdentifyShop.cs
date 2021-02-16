cf171a0b9820612babc908310904368411b43c46
KentKaze <kentnewbie@hotmail.com>
2021-02-16 21:12:37 +0900

    U

---
+++ Aritiafel/Locations/IdentifyShop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aritiafel.Locations
+{
+    public static class IdentifyShop
+    {
+        public static long Counter { get; set; } = 0;
+        public static string GetNewID(string preposition = "", bool resetCounter = false)
+        {
+            if (resetCounter)
+                Counter = 0;
+            string s = DateTime.Now.Ticks.ToString();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+                if(i != s.Length - 1)
+                    result.Append((char)(s[i + 1] | s[i]));
+            result.Append(Counter++);
+            return result.ToString();
+        }
+    }
+}
---
+++ AritiafelTestFormTests/IdentifyShopTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Aritiafel.Locations;
+
+namespace AritiafelTestFormTests
+{
+    [TestClass]
+    public class IdentifyShopTest
+    {
+        [TestMethod]
+        public void GetNewID()
+        {
+            
+        }
+    }
+}
