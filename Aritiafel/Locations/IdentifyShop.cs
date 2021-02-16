cc42f7b81c9f52d59d37ac2175c9c7f87f9fe728
KentKaze <kentnewbie@hotmail.com>
2021-02-16 21:54:50 +0900

    U

--- Aritiafel/Locations/IdentifyShop.cs
+++ Aritiafel/Locations/IdentifyShop.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Aritiafel.Locations
 {
     public static class IdentifyShop
     {
+        public static bool NoPause { get; set; }
         public static long Counter { get; set; } = 0;
+        private static char GetLetterOrDigitFromRandom62(int i)
+        {
+            if (i < 10)
+                return (char)(i + 48);
+            else if (i < 36)
+                return (char)(i + 55);
+            else
+                return (char)(i + 61);
+        }
+
         public static string GetNewID(string preposition = "", bool resetCounter = false)
         {
             if (resetCounter)
                 Counter = 0;
-            string s = DateTime.Now.Ticks.ToString();
             StringBuilder result = new StringBuilder();
-            for (int i = 0; i < s.Length; i++)
-                if(i != s.Length - 1)
-                    result.Append((char)(s[i + 1] | s[i]));
+            result.Append(preposition);
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+            if(!NoPause)
+                Thread.Sleep(1);
+            result.Append(GetLetterOrDigitFromRandom62(rnd.Next(10, 62)));
+            for (int i = 0; i < 10; i++)
+                result.Append(GetLetterOrDigitFromRandom62(rnd.Next(62)));
             result.Append(Counter++);
             return result.ToString();
         }
--- AritiafelTestFormTests/IdentifyShopTest.cs
+++ AritiafelTestFormTests/IdentifyShopTest.cs
@@ -7,10 +7,29 @@ namespace AritiafelTestFormTests
     [TestClass]
     public class IdentifyShopTest
     {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void RandomTest()
+        {   
+            
+            TestContext.WriteLine(DateTime.Now.Ticks.ToString());
+            TestContext.WriteLine(DateTime.Now.Ticks.ToString());
+            TestContext.WriteLine(((int)DateTime.Now.Ticks).ToString());
+            
+            TestContext.WriteLine(DateTime.Now.Ticks.ToString());
+            TestContext.WriteLine(((int)DateTime.Now.Ticks).ToString());
+            
+        }
+
         [TestMethod]
         public void GetNewID()
         {
-            
-        }
+            TestContext.WriteLine(IdentifyShop.GetNewID());
+            TestContext.WriteLine(IdentifyShop.GetNewID());
+            TestContext.WriteLine(IdentifyShop.GetNewID());
+            TestContext.WriteLine(IdentifyShop.GetNewID());
+            TestContext.WriteLine(IdentifyShop.GetNewID());
+        }       
     }
 }
