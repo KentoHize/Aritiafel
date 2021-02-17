83797ed3ab9f9c8595562191d09897bf24f8d5c9
KentKaze <kentnewbie@hotmail.com>
2021-02-18 03:58:12 +0900

    U

---
+++ Aritiafel/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aritiafel
+{
+    //待整理
+    public static class Extensions
+    {
+        public static double NextRandomDouble(this Random rnd)
+        {   
+            byte[] bytesArray = new byte[8];
+            for (int i = 0; i < 8; i++)
+                bytesArray[i] = (byte)rnd.Next(byte.MaxValue + 1);
+            return BitConverter.ToDouble(bytesArray, 0);
+        }
+
+        public static double NextRandomDouble(this Random rnd, double minValue, double maxValue)
+        {
+            if (minValue > float.MinValue && maxValue < float.MaxValue)
+                return rnd.NextDouble() * (maxValue - minValue) + minValue;
+            double d;
+            do
+            {
+                d = NextRandomDouble(rnd);
+            }
+            while (d > minValue && d < maxValue);
+            return d;
+        }
+    }
+}
