d2a8523d0c43641841291008a812679141f9a4f7
KentKaze <kentnewbie@hotmail.com>
2021-02-25 22:43:34 +0900

    U

---
+++ AritiafelTests/ExtensionTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.Json;
+using Aritiafel.Locations.StorageHouse;
+using System.Collections.Generic;
+using Aritiafel;
+
+namespace AritiafelTestFormTests
+{
+    [TestClass]
+    public class ExtensionTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void RandomDoubleTest()
+        {
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+            rnd.NextRandomDouble();
+            double d;
+            for(int i = 0; i < 1000; i++)
+                d = rnd.NextRandomDouble(double.MinValue / 10, double.MaxValue / 10);
+            
+            for (int i = 0; i < 1000; i++)
+            {                
+                d = rnd.NextRandomDouble(double.MinValue / Math.Pow(10, 100), double.MaxValue / Math.Pow(10, 100));
+                if (i < 20)
+                    TestContext.WriteLine(d.ToString());
+            }
+                
+
+        }
+    }
+}
